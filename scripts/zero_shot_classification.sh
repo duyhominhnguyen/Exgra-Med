cd06cdd23ed5d44e719d471cc63cf5a3d51598d5
TrungQuocNguyen <trungng28296@gmail.com>
2025-12-16 22:49:06 +0100

    Add zero-shot classification script

---
+++ scripts/zero_shot_classification.sh
@@ -0,0 +1,14 @@
+FILENAME='NLM- Malaria Data.json' # json file of the corresponding data, stored in OmniMedVQA/QA_information/Open-access
+OUTPUTNAME=NLM-Malaria # name of result folder
+WORKDIR=$(pwd) # where OmniMedVQA folder is extracted
+MODEL_CKPT=checkpoint_llava_med_instruct_60k_inline_mention_version_1-5_1e0_multi_graph_100_scale_dci_test_bugfix # path to folder checkpoint
+
+
+export PYTHONPATH=$PYTHONPATH:./exgra_med
+python exgra_med/llava/eval/model_omnimed_vqa_eval.py \
+--question-file "$WORKDIR/OmniMedVQA/QA_information/Open-access/$FILENAME" \
+--model-name $MODEL_CKPT \
+--mm_dense_connector_type dci \
+--num_l 6 \
+--image-folder "$WORKDIR/OmniMedVQA" \
+--answers_base_path OmnimedVQA_results/$OUTPUTNAME
